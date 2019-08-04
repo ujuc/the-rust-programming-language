76126c27c38288aec1e323eb4eb6ba0f8412fc82
Thomas SJ Kang <ujuc@ujuc.kr>
2019-08-04 11:33:03 +0900

    chapter 3 - 7

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
-    let number = 6;
+    let a = [10, 20, 30, 40, 50];
 
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
+    for element in a.iter() {
+        println!("The value is: {}", element);
     }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+
+    println!("LIFTOFF!!!");
 }
