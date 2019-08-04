691b1534346b9f747b595c7d7f1a0030dfe052c5
Thomas SJ Kang <ujuc@ujuc.kr>
2019-08-04 11:27:22 +0900

    chapter 3 - 5

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let x = plus_one(5);
+    let number = 3;
 
-    println!("The value of x is: {}", x);
-}
-
-fn plus_one(x: i32) -> i32 {
-    x + 1
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
