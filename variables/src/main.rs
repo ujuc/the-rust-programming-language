14c09ed64000630746a861e3f9adaeff894f19eb
Thomas SJ Kang <ujuc@ujuc.kr>
2019-08-04 11:17:12 +0900

    chapter 3 - 2

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
     println!("The value of x is: {}", x);
 }
