d9fce3bf8719a162ec3decbe6a2418c6ee26cd5d
Thomas SJ Kang <ujuc@ujuc.kr>
2019-08-06 22:28:35 +0900

    ch5-16.1

--- struct_lessen/src/main.rs
+++ struct_lessen/src/main.rs
@@ -14,6 +14,12 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
@@ -26,5 +32,8 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    println!("Square {:#?}", sq);
 }
 
