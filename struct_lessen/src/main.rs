68cdb49ab52510a1a05ef6eb92a780ee0835970f
Thomas SJ Kang <ujuc@ujuc.kr>
2019-08-06 22:15:02 +0900

    ch5-12

--- struct_lessen/src/main.rs
+++ struct_lessen/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -6,12 +7,6 @@ struct Rectangle {
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    println!("rect1 is {:#?}", rect1);
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
