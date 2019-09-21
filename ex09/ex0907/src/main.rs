1c5c893cbd038312d028457b4cc3afb2efab5c2f
Thomas SJ Kang <ujuc@ujuc.kr>
2019-09-21 13:14:02 +0900

    ch09

---
+++ ex09/ex0907/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+
+    f.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn main() {
+    let s = read_username_from_file();
+
+    println!("{:?}", s);
+}
